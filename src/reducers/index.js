b15ff640678e58509363723955d6c12d1aea95dd
bettercallazamat <bettercallazamat@gmail.com>
2021-09-07 22:59:44 +0600

    Refactor rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,9 @@ import { combineReducers } from 'redux';
 import bookReducer from './books';
 import filterReducer from './filter';
 
-const rootReducer = combineReducers({ bookReducer, filterReducer });
+const rootReducer = combineReducers({
+  bookReducer,
+  filterReducer 
+});
 
 export default rootReducer;
